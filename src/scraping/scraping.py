22faa4ebf94a8e65475f57e2d2fd1b77a29a11cb
Greg Lyons <gregory.lyons512@gmail.com>
2022-05-11 11:40:48 -0500

    Wrote scraping code.

--- src/scraping/scraping.py
+++ src/scraping/scraping.py
@@ -1,51 +1,58 @@
 import csv
 
-from utils import getDataPath, openLink
+from utils import getDataPath, openLink, parseName
 
 
 def makeEncounterCSV(fname):
-    # Navigate to link
-    bs = openLink(
-        'https://bulbapedia.bulbagarden.net/wiki/List_of_locations_by_index_number_(Generation_I)', 0, 10)
-
-    # Section where desired table is
-    findSection = bs.find('span', id=f'List_of_locations_by_index_number')
-
-    # Desired table (nested within an outer table)
-    findTable = findSection.findNext('table').find('table')
-
-    # Keep track of links already visited
-    visitedHrefs = set()
-
-    # Select rows of table
-    rows = findTable.find_all('tr')
-    for row in rows:
-        cells = row.find_all('td')
-
-        # Ignore rows with less than 4 cells; don't contain useful information
-        # Ignore rows with no links
-        if len(cells) != 4 or row.find('a') == None:
-            continue
-
-        # Cell with desired data
-        locationCell = cells[2]
-
-        # Visit each link
-        for locationLink in locationCell.find_all('a'):
-            # If link has been visited, move on
-            if locationLink['href'] in visitedHrefs:
-                continue
-            # Otherwise, add encounter data
-            visitLocationLink(locationLink)
-            visitedHrefs.add(locationLink['href'])
 
     with open(fname, 'w', newline='', encoding='utf-8') as csvFile:
         writer = csv.writer(csvFile)
+        writer.writerow(['Location', 'Pokemon', 'R', 'B',
+                        'Y', 'Method', 'Levels', 'Rate'])
+
+        # Navigate to link
+        bs = openLink(
+            'https://bulbapedia.bulbagarden.net/wiki/List_of_locations_by_index_number_(Generation_I)', 0, 10)
+
+        # Section where desired table is
+        findSection = bs.find('span', id=f'List_of_locations_by_index_number')
+
+        # Desired table (nested within an outer table)
+        findTable = findSection.findNext('table').find('table')
+
+        # Keep track of links already visited
+        visitedHrefs = set()
+
+        # Select rows of table
+        rows = findTable.find_all('tr')
+        for row in rows:
+            cells = row.find_all('td')
+
+            # Ignore rows with less than 4 cells; don't contain useful information
+            # Ignore rows with no links
+            if len(cells) != 4 or row.find('a') == None:
+                continue
+
+            # Cell with desired data
+            locationCell = cells[2]
+
+            # Visit each link
+            for locationLink in locationCell.find_all('a'):
+                # If link has been visited, move on
+                if locationLink['href'] in visitedHrefs:
+                    continue
+                # Otherwise, add encounter data
+                visitLocationLink(locationLink, writer)
+                visitedHrefs.add(locationLink['href'])
 
     return
 
 
-def visitLocationLink(locationLink):
+def visitLocationLink(locationLink, writer):
+    #
+    # Parse locationLink to determine if it has encounter data; if so, find section where encounter data is given
+    # region
+
     locationName = locationLink.get_text()
     locationPage = openLink(
         'https://bulbapedia.bulbagarden.net' + locationLink['href'], 0, 10)
@@ -67,20 +74,63 @@ def visitLocationLink(locationLink):
         return False
 
     # Otherwise, find desired gen
-    print(locationName)
     findTableSection = encounterSection.find_next('span', id=f'Generation_I')
 
     # If no table for desired gen, leave
     if findTableSection == None:
         return False
 
+    #
+    # endregion
+    #
+
+    #
+    # Add table data to .csv
+    # region
     findTable = findTableSection.find_next('table')
-    rows = findTable.find('tbody').findChildren('tr', recursive=False)
-    print(rows)
+
+    # Parse header row to get number of games being considered
+    headerRow = findTable.find('tr')
+    numberGames = 1
+    for th in headerRow.findChildren(['th'], recursive=False):
+        if 'Games' in th.get_text():
+            numberGames = int(int(th['colspan']) / 2)
+
+    rows = findTable.find('tbody').findChildren('tr', recursive=False)[1:]
     for row in rows:
+
         cells = row.findChildren(['td', 'th'], recursive=False)
 
-        print(len(cells))
+        # Ignore rows which are headers for sections/don't have enough cells
+        if (len(cells) < 4 + numberGames):
+            continue
+
+        csvRow = [locationName]
+
+        pokemonName = parseName(cells[0].get_text(
+        ).strip('\n'))
+        csvRow.append(pokemonName)
+
+        # Get presence of data in games
+        games = cells[1:1+numberGames]
+        for game in games:
+            # White background indicates not present in game
+            if game['style'] in ['background:#FFF;', 'background:#FFFFFF;']:
+                csvRow.append(False)
+            else:
+                csvRow.append(True)
+
+        location = ' '.join(list(cells[-3].stripped_strings))
+        csvRow.append(location)
+        levels = ' '.join(list(cells[-2].stripped_strings))
+        csvRow.append(levels)
+        rate = ' '.join(list(cells[-1].stripped_strings))
+        csvRow.append(rate)
+
+        writer.writerow(csvRow)
+    #
+    # endregion
+    #
 
     return True
 
